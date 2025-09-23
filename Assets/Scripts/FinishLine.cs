4a4ed5fcb867bb3cd96c7fad4d611af10b107206
BRUNO TORRES <39955941820@info.local>
2025-09-23 17:37:42 -0300

    Novos carros adds na pista e balanceamento da IA dos oponentes

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,32 +2,14 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    private RaceManager raceManager;
-
-    void Start()
-    {
-        raceManager = FindObjectOfType<RaceManager>();
-        if (raceManager == null)
-        {
-            Debug.LogError("FinishLine: Nenhum RaceManager encontrado na cena!");
-        }
-    }
-
-    private void OnTriggerEnter(Collider other) // se for 3D
+    void OnTriggerEnter2D(Collider2D other)
     {
         DragCar car = other.GetComponent<DragCar>();
-        if (car != null && raceManager != null)
+        if (car != null && car.raceStarted)
         {
-            raceManager.FinishRace(car);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) // se for 2D
-    {
-        DragCar car = other.GetComponent<DragCar>();
-        if (car != null && raceManager != null)
-        {
-            raceManager.FinishRace(car);
+            RaceManager rm = FindObjectOfType<RaceManager>();
+            if (rm != null)
+                rm.FinishRace(car);
         }
     }
 }
